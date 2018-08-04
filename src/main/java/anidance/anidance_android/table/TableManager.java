f6dc0491514bd4b56eb6a357bbdf9308dd2c3759
nodgd <dcc1234@qq.com>
2018-08-05 01:25:38 +0800

    降低了重复动作的概率

--- src/main/java/anidance/anidance_android/table/TableManager.java
+++ src/main/java/anidance/anidance_android/table/TableManager.java
@@ -1,6 +1,7 @@
 package anidance.anidance_android.table;
 
 import android.content.Context;
+import android.util.Log;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -23,6 +24,8 @@ public class TableManager {
     private int beats;
     private String lastStep;
 
+    private String[] mNearestStepList = new String[16];
+
     public TableManager(String dance_type) {
         this.dict = new Dict();
         Edges e = new Edges();
@@ -34,7 +37,7 @@ public class TableManager {
         this.beats = 0;
         this.startstep = new HashMap<>();
         startstep.put("C", Arrays.asList("C-0-1", "C-0-2", "C-0-3", "C-0-4", "C-1-1", "C-11-2", "C-25-1"));
-        startstep.put("T", Arrays.asList("START"));
+        startstep.put("T", Arrays.asList("T-1-1", "T-14-1", "T-16-2", "T-2-3", "T-4-1", "T-5-1", "T-8-1"));
         startstep.put("W", Arrays.asList("W-0-2", "W-1-1", "W-2-1"));
         startstep.put("R", Arrays.asList("R-0-3", "R-0-4", "R-0-5", "R-0-6", "R-10-1", "R-11-1", "R-14-1", "R-22-3"));
         lastStep = null;
@@ -47,7 +50,8 @@ public class TableManager {
         }
         String bad = "";
         String s = "";
-        double max = 0;
+        double max = Double.MIN_VALUE;
+        Log.d(TAG, "choose_next: step = " + step + ", tmp = " + tmp);
         for (String w : tmp.keySet()) {
             if (w.equals("HOLD") || motions.get(w).is_terminal) {
                 bad = w;
@@ -60,7 +64,12 @@ public class TableManager {
                     prob = prior;
                 } else {
                     for (int i = 0; i < 13; ++i) {
-                        prob = Math.max(prob, (mu.get(i) - mfcc.get(i)) * sigma.get(i));
+                        prob += Math.exp((mu.get(i) - mfcc.get(i)) / sigma.get(i));
+                    }
+                    for (int j = 0; j < mNearestStepList.length; j ++) {
+                        if (mNearestStepList[j] != null && w.equals(mNearestStepList[j])) {
+                            prob *= 0.7;
+                        }
                     }
                     if (max < prob) {
                         s = w;
@@ -69,11 +78,13 @@ public class TableManager {
                 }
             }
         }
-        if (max == 0) {
-            return bad;
-        } else {
-            return s;
+        if (max != Double.MIN_VALUE) {
+            for (int i = mNearestStepList.length - 1; i >= 1; i--) {
+                mNearestStepList[i] = mNearestStepList[i - 1];
+            }
+            mNearestStepList[0] = s;
         }
+        return s;
     }
 
     public void reset() {
